8444a681163540959e5e7ae0e345c02e37652a50
BROWNFOX\derek <brownfoxsir@gmail.com>
2021-01-11 23:58:50 +0800

    chap1

---
+++ src/main/hello_world.go
@@ -0,0 +1,16 @@
+package main // 包，表明代码所在的模块（包）
+
+import (
+	"fmt"
+	"os"
+) // 引入代码依赖
+
+// 功能实现
+func main() {
+	fmt.Println(os.Args)
+	if len(os.Args) > 1 {
+
+		fmt.Println("hello World", os.Args[1])
+	}
+	os.Exit(0)
+}
